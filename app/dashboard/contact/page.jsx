ec7291e14103e71b6b37337f9257ba9df1cd4539
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-07-05 16:49:33 +0500

    .

--- app/dashboard/contact/page.jsx
+++ app/dashboard/contact/page.jsx
@@ -5,14 +5,14 @@ const page = () => {
   return (
     <div className="flex flex-col gap-5 mt-6 ">
       <div>
-        <h2 className="text-2xl text-gray-500 mb-1">Let us Know</h2>
+        <h2 className="text-2xl text-gray-600 mb-1 font-semibold">Let us Know</h2>
         <div className="flex ">
           <Dot />
-          <h2 className="text-lg ">About any issues in the website.</h2>
+          <h2 className=" text-gray-700 ">About any issues in the website.</h2>
         </div>
         <div className="flex ">
           <Dot />
-          <h2 className="text-lg ">Your Feedback.</h2>
+          <h2 className="text-gray-700 ">Your Feedback.</h2>
         </div>
       </div>
       <SendMessage />
