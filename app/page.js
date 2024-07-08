023dad54fe2a9d818fc40cab141c622e5183853f
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-07-09 02:16:33 +0500

    .

--- app/page.js
+++ app/page.js
@@ -3,13 +3,11 @@ import { redirect } from 'next/navigation';
 
  const Home=async()=> {
   const session= await auth()
-  if(session?.user) redirect("/sign-in")
-    
-  return (
-    <div className="h-[70vh] w-full flex justify-center items-center font-semibold text-3xl text-gray-500">
-   hello
-    </div>
-  );
+  if(session?.user){
+    redirect("/sign-in")
+  }else{
+    redirect("/dashboard")
+  }
 }
 
 export default Home
