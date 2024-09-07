fdd5d0df32205ad5816af53f19274d9f9839a7cc
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-09-07 17:58:11 +0500

    .

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://MockAI_owner:QT9hXvlBLY6w@ep-billowing-dust-a1f97z9q.ap-southeast-1.aws.neon.tech/MockAI?sslmode=require',
+      url: NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
   };
